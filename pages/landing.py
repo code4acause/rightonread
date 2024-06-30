b96280961d65970db4dfab3e57267357d058b978
Gunnar Eklund <105998814+gweklund711@users.noreply.github.com>
2024-06-30 16:18:37 -0400

    Update landing.py

--- pages/landing.py
+++ pages/landing.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
+st.title("RightOnRead")
 st.write("Welcome to the Book Club!")
+st.write("RightOnRead is a website that incentivizes people to read. By using a dynamic competition system, RightOnRead aims to educate children and promote literacy across the country.")
+st.header("How do competitions work?")
+st.subheader("Different Roles:")
+st.write("Hosts are able to create and share competitions. Users are able to view, join, and compete in competitions.")
